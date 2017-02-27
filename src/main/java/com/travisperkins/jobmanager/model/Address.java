96568ebce491891a5a96f41f27d5ec621ed19cd3
Mohamed Samandi <msamandi@andigital.com>
2017-02-27 11:47:47 +0000

    Test

---
+++ src/main/java/com/travisperkins/jobmanager/model/Address.java
@@ -0,0 +1,7 @@
+package com.travisperkins.jobmanager.model;
+
+/**
+ * Created by msamandi on 26/02/2017.
+ */
+public class Address {
+}
---
+++ src/main/java/com/travisperkins/jobmanager/model/Quote.java
@@ -0,0 +1,32 @@
+package com.travisperkins.jobmanager.model;
+
+/**
+ * Created by msamandi on 23/02/2017.
+ */
+public class Quote {
+    public Quote(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String name ;
+private String type ;
+private String description ;
+
+}
