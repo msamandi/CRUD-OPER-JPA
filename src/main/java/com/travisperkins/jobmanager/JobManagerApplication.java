4af13b48e12fed5543f8b666070d0c49d25f124b
Mohamed Samandi <msamandi@andigital.com>
2017-02-28 11:56:28 +0000

    Test db to h2 with flyway

--- src/main/java/com/travisperkins/jobmanager/JobManagerApplication.java
+++ src/main/java/com/travisperkins/jobmanager/JobManagerApplication.java
@@ -1,7 +1,15 @@
 package com.travisperkins.jobmanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by ppop on 22/02/2017.
@@ -12,4 +20,18 @@ public class JobManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobManagerApplication.class);
     }
+
+    /*@Bean
+    public CommandLineRunner runner(final PersonRepository repository) {
+        return new CommandLineRunner() {
+
+            @Override
+            public void run(String... args) throws Exception {
+                System.err.println(repository.findAll());
+            }
+
+        };
+    }*/
+
+
 }
